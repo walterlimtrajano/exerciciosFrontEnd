15856e93f2671aacd5a8aac1d73ff4ff8d66bdf5
walterlimtrajano <128225888+walterlimtrajano@users.noreply.github.com>
2025-09-05 16:07:08 -0300

    aprendendo a usar js

---
+++ aula1/index.js
@@ -0,0 +1,37 @@
+//console.log("Olá, mundo!");
+
+//const nome = "Walter";
+//console.log("Meu nome é " + nome);
+
+//const idade = 18; 
+
+//if (idade >= 18) {
+  //  console.log("Você é maior de idade.");
+//} else if (idade === 18) {
+  //  console.log("Você acabou de se tornar maior de idade.");
+//} else {
+  //  console.log("Você é menor de idade.");
+//}
+
+//let i=0;
+//while (i < 10) {
+  //  console.log("Contagem: " + i);
+    //i++;
+//}
+
+//for (let i = 0; i < 10; i++) {
+  //  console.log("Olá, mundo!");
+//}
+
+//const nota1 = 7;
+//const nota2 = 8;
+//const nota3 = 6;
+//const nota4 = 9;
+
+//const media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+//console.log("A média das notas é:", media);
+
+const media = (7 + 8 + 6 + 9) / 4;
+
+console.log("A média das notas é:", media);
