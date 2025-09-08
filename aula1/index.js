ac612acf0d770465dd2a80bd8baf1ecb50cb337f
walterlimtrajano <128225888+walterlimtrajano@users.noreply.github.com>
2025-09-08 17:25:19 -0300

    .

--- aula1/index.js
+++ aula1/index.js
@@ -69,23 +69,46 @@ const prompt = PromptSync();
 
 //console.log("A soma de " + x + " e " + y + " é: " + soma(x, y));
 
-function ehNumeroPar(numero) {
-    return numero % 2 === 0;
-}
+//function ehNumeroPar(numero) {
+  //  return numero % 2 === 0;
+//}
 
-const ehPar = Number(prompt("Digite um número para verificar se é par ou ímpar: "));
+//const ehPar = Number(prompt("Digite um número para verificar se é par ou ímpar: "));
 
-if (ehNumeroPar(ehPar)) {
-    console.log(ehPar + " é um número par. (true)");
-} else {
-    console.log(ehPar + " é um número ímpar. (false)");
-}
+//if (ehNumeroPar(ehPar)) {
+  //  console.log(ehPar + " é um número par. (true)");
+//} else {
+  //  console.log(ehPar + " é um número ímpar. (false)");
+//}
 
-function inverter(palavra) {
-    return palavra.split('').reverse().join('');
-}
+//function inverter(palavra) {
+  //  return palavra.split('').reverse().join('');
+//}
 
 // Exemplo de uso:
-const texto = prompt("Digite uma palavra para inverter: ");
-console.log("Invertida:", inverter(texto));
+//const texto = prompt("Digite uma palavra para inverter: ");
+//console.log("Invertida:", inverter(texto));
+
+const pessoa = {
+  nome: "Walter",
+  idade: 18,
+  profissao: "Estudante"
+};
+
+// 2. Mostre cada valor no console.
+console.log("Nome:", pessoa.nome);
+console.log("Idade:", pessoa.idade);
+console.log("Profissão:", pessoa.profissao);
+
+// 3. Adicione uma nova propriedade cidade ao objeto pessoa.
+pessoa.cidade = "João Pessoa";
+
+// 4. Modifique o valor da propriedade idade para um novo número.
+pessoa.idade = 25;
+
+// 5. Remova a propriedade profissao do objeto.
+delete pessoa.profissao;
+
+// Mostre o objeto final
+console.log("Objeto final:", pessoa);
 
